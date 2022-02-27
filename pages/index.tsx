b563d2af7cc47b105fb92e92a4d67660c51e1914
Luna Cuevas <68247976+luna-cuevas@users.noreply.github.com>
2022-02-27 12:26:40 -0800

    deploying app

---
+++ pages/index.tsx
@@ -0,0 +1,34 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import Sidebar from '../components/Sidebar'
+import Center from '../components/Center'
+import Player from '../components/Player'
+import { getSession } from 'next-auth/react'
+
+const Home: NextPage = () => {
+  return (
+    <div className="bg-black h-screen overflow-hidden">      
+      <main className='flex'>
+        <Sidebar />
+        <Center />
+      </main>
+      
+      <div className='sticky bottom-0'>
+        <Player />
+      </div>
+    </div>
+  )
+}
+
+export default Home
+
+export async function getServerSideProps(context: any) {
+  const session = await getSession(context);
+
+  return {
+    props: {
+      session,
+    }
+  }
+}
